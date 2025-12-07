e9dbeb96a44779e7adc78d0e423e66e03090a46e
Olivier <78881889+KermitPlaysCode@users.noreply.github.com>
2025-12-07 19:38:57 +0100

    Set output type

--- lib_chrono.py
+++ lib_chrono.py
@@ -16,7 +16,7 @@ class chrono:
         self._flag_e = False
         self._delay = 0
 
-    def start(self) -> None:
+    def start(self) -> int:
         """Set beginning of measure"""
         ret = E_OK
         if not self._flag_s:
@@ -27,7 +27,7 @@ class chrono:
             ret = E_ALREADY_STARTED
         return ret
 
-    def stop(self) -> None:
+    def stop(self) -> int:
         """Set end of measure"""
         ret = E_OK
         if self._flag_s:
